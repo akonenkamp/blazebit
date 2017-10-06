cfdf7527d6a626a7707bda7d6b9aede108f1c53c
Amanda Konenkamp <amanda.konenkamp@libertymutual.com>
2017-10-06 11:08:34 -0700

    first commit to init repo

---
+++ src/main/java/com/libertymutual/goforcode/blazebit/BlazebitApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.goforcode.blazebit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazebitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlazebitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/libertymutual/goforcode/blazebit/BlazebitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.libertymutual.goforcode.blazebit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlazebitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
